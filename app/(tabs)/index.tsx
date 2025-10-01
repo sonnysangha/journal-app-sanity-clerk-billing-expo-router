0481ebd404880321a17d1ffddfde2702cf6e79d2
Sonny Sangha <sonny.sangha@gmail.com>
2025-10-01 14:34:18 +0400

    Refactor layout and index files to improve code consistency and readability; replace string quotes with double quotes, update component imports, and enhance the HomeScreen layout with new Tamagui components and links.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,8 @@
-import { Image } from "expo-image";
-import { Platform, StyleSheet } from "react-native";
-
-import { HelloWave } from "@/components/hello-wave";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
+import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Button } from "tamagui";
+import { StyleSheet } from "react-native";
+import { Button, H1, H5, H6, Text, View } from "tamagui";
 
 export default function HomeScreen() {
   return (
@@ -19,73 +15,29 @@ export default function HomeScreen() {
         />
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction
-              title="Action"
-              icon="cube"
-              onPress={() => alert("Action pressed")}
-            />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert("Share pressed")}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert("Delete pressed")}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+      <H1>Welcome!</H1>
+      <H5>To Journal.ai!</H5>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <Button theme="blue">Click me</Button>
+      <View>
+        <Text marginBlockEnd={16}>
+          Journal.ai is your AI-powered journal assistant. It helps you track
+          your mood, thoughts, and feelings.
+        </Text>
 
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+        {/* Divider */}
+        <View style={styles.divider} />
+
+        <Text marginBlockEnd={8}>From Sonny Sangha</Text>
+        <H6>#PAPAFAM</H6>
+      </View>
+
+      <Link href="https://www.youtube.com/sonnysangha" asChild>
+        <Button theme="blue">Watch the YouTube Video</Button>
+      </Link>
+
+      <Link href="https://www.papareact.com/journal-ai-form" asChild>
+        <Button theme="red">Get the code here</Button>
+      </Link>
     </ParallaxScrollView>
   );
 }
@@ -106,5 +58,10 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: "absolute",
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "lightgray",
+    marginVertical: 16,
   },
 });
